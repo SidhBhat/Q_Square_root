13bddb9bfab50120af837bc4d23c39f4a2dce57a
Siddharth Bhat <essdeehb@gmail.com>
2023-03-04 12:29:24 +0530

    Updated original referance code

--- Original/fast_inverse_square_root.c
+++ Original/fast_inverse_square_root.c
@@ -1,7 +1,8 @@
 /* THE FAST INVERSE SQUARE ROOT ALGORITHM */
 
-// The original Version, copied as is.
-// Computes the inverse square root x^(-1/2)
+/* The original Version, copied as is. */
+/* Computes the inverse square root x^(-1/2) */
+
 float Q_rsqrt( float number )
 {
 	long i;
@@ -10,12 +11,11 @@ float Q_rsqrt( float number )
 
 	x2 = number * 0.5F;
 	y  = number;
-
-	i  = * ( long * ) &y;               // evil floating point bit level hacking
-	i  = 0x5f3759df - ( i >> 1 );       // what the fuck?
+	i  = * ( long * ) &y;						// evil floating point bit level hacking
+	i  = 0x5f3759df - ( i >> 1 );               // what the fuck?
 	y  = * ( float * ) &i;
 	y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
-//	y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
+	//	y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
 
 	return y;
 }
